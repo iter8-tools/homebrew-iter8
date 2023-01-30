5a66dd35b3dca1ccf59a714646582894aaf6889f
sriumcp <sriumcp@users.noreply.github.com>
2023-01-30 21:13:31 +0000

    Apply changes to brew formula

--- iter8.rb
+++ iter8.rb
@@ -2,13 +2,13 @@ class Iter8 < Formula
   desc "Kubernetes release engineering"
   homepage "https://iter8.tools"
   url "https://github.com/iter8-tools/iter8.git",
-      tag:      "v0.13.4",
-      revision: "d5f1b66f390c39e905268f283c33967388d4d2d4"
+      tag:      "v0.13.5",
+      revision: "3ddfc5cced50523cb98f4f544be69e7dc966ebf0"
   license "Apache-2.0"
 
   if OS.mac?
-    url "https://github.com/iter8-tools/iter8/releases/download/v0.13.4/iter8-darwin-amd64.tar.gz"
-    sha256 "e8d21a50970874562c438f3a74be5b74315889c5ea452475e1cb2625ec416a82"
+    url "https://github.com/iter8-tools/iter8/releases/download/v0.13.5/iter8-darwin-amd64.tar.gz"
+    sha256 "1c7a4038c4197ac52774e35397680df9cd33ceff1163e4c85ae2b78685819d70"
   else
     depends_on "go" => :build
   end
